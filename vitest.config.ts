403d807bf0ca0ed063dc279eb5df96730cc28772
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-01-24 16:41:12 +0100

    fix vite config, add stryker command, update gitignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    include: ["test/vitest/**/*.{spec,test}.{js,ts}"],
+    include: ["test/**/*.{spec,test}.{js,ts}"],
     coverage: {
       enabled: true,
       provider: "v8",
