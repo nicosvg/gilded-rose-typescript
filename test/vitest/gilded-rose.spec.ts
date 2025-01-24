58bf9da32b84a5861fac59d1c64a63a700b5d705
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-01-24 16:26:30 +0100

    init repo

---
+++ test/vitest/gilded-rose.spec.ts
@@ -0,0 +1,13 @@
+import { describe } from "vitest";
+import { GildedRose, Item } from "../app/gilded-rose";
+
+describe.skip("Gilded Rose", () => {
+  it("should foo", () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+    // Act
+    const items = gildedRose.updateQuality();
+    // Assert
+    expect(items[0].name).toBe("bar");
+  });
+});
