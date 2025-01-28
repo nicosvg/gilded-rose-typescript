f1a232039e11a85b07f77a6d67cf66f852594572
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2025-01-28 11:32:00 +0100

    add snapshot tests

---
+++ test/snapshot-test.ts
@@ -0,0 +1,37 @@
+import { Item, GildedRose } from "../app/gilded-rose";
+
+console.log("OMGHAI!");
+
+const items = [
+  new Item("+5 Dexterity Vest", 10, 20), //
+  new Item("Aged Brie", 2, 20), //
+  new Item("Elixir of the Mongoose", 5, 7), //
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  // this conjured item does not work properly yet
+  new Item("Conjured Mana Cake", 3, 16),
+];
+
+const gildedRose = new GildedRose(items);
+
+let days: number = 20;
+if (process.argv.length > 2) {
+  days = +process.argv[2];
+}
+
+export function runTest(days: number) {
+  const logs: string[] = [];
+  for (let i = 0; i < days; i++) {
+    logs.push("-------- day " + i + " --------");
+    logs.push("name, sellIn, quality");
+    items.forEach((element) => {
+      logs.push(element.name + ", " + element.sellIn + ", " + element.quality);
+    });
+    logs.push();
+    gildedRose.updateQuality();
+  }
+  return logs;
+}
